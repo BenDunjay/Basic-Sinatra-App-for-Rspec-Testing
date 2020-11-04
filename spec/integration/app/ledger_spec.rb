5713cae7a670d65ce4fa3bb5b23236ee7954c79d
BenDunjay <bendunjay@outlook.com>
2020-11-04 14:59:25 +0000

    ledger spec set up

---
+++ spec/integration/app/ledger_spec.rb
@@ -0,0 +1,19 @@
+require_relative "../../../app/ledger"
+require_relative "../../../config/sequel"
+require_relative "../../../support/db"
+
+module ExpenseTracker
+  RSpec.describe Ledger do
+    let(:ledger) { Ledger.new }
+    let(:expense) do
+      {
+              "payee" => "Starbucks",
+              "amount" => 5.75,
+              "date" => "2017-06-10",
+            }
+    end
+
+    describe "#record" do
+    end
+  end # Rspec describe end
+end # module end
