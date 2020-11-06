4504a56f63993ea6322a91197762c821186c304f
BenDunjay <bendunjay@outlook.com>
2020-11-06 14:46:33 +0000

    update

--- app/ledger.rb
+++ app/ledger.rb
@@ -3,9 +3,13 @@ module ExpenseTracker
 
   class Ledger
     def record(expense)
+      DB[:expenses].insert(expense)
+      id = DB[:expenses].max(:id)
+      RecordResult.new(true, id, nil)
     end
 
     def expenses_on(date)
+      DB[:expenses].where(date: date).all
     end
   end # class end
 end # module end
